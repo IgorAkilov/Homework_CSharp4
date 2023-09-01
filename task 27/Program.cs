5f85a0eca700b0d55bd26346af95ea181062ceb8
Igor Akilov <akilovim@gmail.com>
2023-09-01 11:54:33 +0700

    added task 27

---
+++ task 27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, 
+//которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Prompt(string message)
+{
+Console.WriteLine(message);
+int num = int.Parse(Console.ReadLine());
+return num;
+}
+
+int number = Prompt("Введите число:\t");
+
+int Count(int num){
+    int result = 0;
+    for(int i = 0; i < num; i++){
+        int temp = num % 10;
+        result += temp;
+        num /= 10;
+        
+    }
+    return result;
+}
+
+int sum = Count(number);
+Console.WriteLine("Сумма цифр числа = " + sum);
